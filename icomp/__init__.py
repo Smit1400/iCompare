73fccc8b13c868703870802564fabcce26c0ebea
Smit1400 <46996928+Smit1400@users.noreply.github.com>
2020-03-01 11:27:46 +0530

    sign up

--- icomp/__init__.py
+++ icomp/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '96e582be6947f9b29b1cd0f615a33600'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from icomp import routes
--- icomp/routes.py
+++ icomp/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template,redirect ,url_for ,flash
 from icomp.forms import SignUpForm,LoginForm
 from icomp.models import User ,News
-from icomp import app
+from icomp import app ,db ,bcrypt
 
 @app.route("/")
 @app.route("/home")
@@ -20,14 +20,22 @@ def products():
 def contact():
 	return render_template('contact.html')
 
-@app.route("/login")
+@app.route("/login" ,methods=['POST'])
 def login():
-	return render_template('login.html')
+	form=LoginForm()
+	if form.validate_on_submit():
+		flash('You have been successfully logged in!' ,'success')
+		return redirect(url_for('home'))
+	return render_template('login.html',form=form)
 
 @app.route("/signup",methods=['GET','POST'])
 def signup():
 	form = SignUpForm()
 	if form.validate_on_submit():
+		hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data,email=form.email.data,password=hashed_pass)
+		db.session.add(user)
+		db.session.commit()
 		flash(f'Account created successfully for  { form.username.data }' ,'success')
 		return redirect(url_for('home'))
 	return render_template('signup.html',form=form)
